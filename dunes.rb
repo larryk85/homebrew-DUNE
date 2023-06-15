70bd61d1460de6fe3f9487f4870d665eceff5d49
Bucky Kittinger <bucky@eosn.foundation>
2023-06-15 19:26:24 -0400

    Update dunes.rb

--- dunes.rb
+++ dunes.rb
@@ -1,18 +1,21 @@
 class Dunes < Formula
-   desc "DUNES"
+   desc "Docker Utilities for Node Execution and Subsystems (DUNES)"
    homepage "https://www.github.com/antelopeio/DUNES"
    revision 0
-   url "https://github.com/antelopeio/dunes/archive/v1.1.2.tar.gz"
+   url "https://github.com/larryk85/homebrew-dunes/releases/download/v1.1.2/dunes.tar.gz"
    version "1.1.2"
-
-   option :universal
-
-   bottle do
-      root_url "https://github.com/larryk85/homebrew-dunes/releases/download/v1.0.0/dunes.tar.gz"
-      sha256 "c4b2c61f65093551019cfbba4f96ee61245a2f4e86bec230f4a7c28b15660bda"
-   end
 
    depends_on "docker"
    depends_on "python3"
+   depends_on "pyinstaller"
+
+   def install
+      system "pip3", "install", "argcomplete", "requests"
+      system "./install.sh " "macos " "."
+   end
+
+   test do
+      system "false"
+   end
 end
 __END__
